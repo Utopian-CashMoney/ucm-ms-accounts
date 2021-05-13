0583d89c03f18473985019d861a56f2510dbfb6d
Joshua Podhola <joshua.podhola@smoothstack.com>
2021-05-12 20:13:07 -0400

    Initial commit; configured H2

---
+++ src/main/java/com/ucm/ms/branches/AccountsMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.ucm.ms.branches;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+// TODO: Configure Spring Security later. Disabled until relevant.
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class AccountsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountsMicroserviceApplication.class, args);
+    }
+
+}
