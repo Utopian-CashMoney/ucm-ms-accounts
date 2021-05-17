a447f052b89bcd0529bd68183a428c293ff7ee57
Josten Asercion <josten.asercion@smoothstack.com>
2021-05-17 04:16:23 -0400

    Finished needed UC-98 services

---
+++ src/main/java/com/ucm/ms/accounts/services/CardSearch.java
@@ -0,0 +1,38 @@
+package com.ucm.ms.accounts.services;
+
+import com.ucm.ms.accounts.dao.CardDAO;
+import com.ucm.ms.accounts.entities.Card;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import javax.transaction.Transactional;
+
+import java.util.Collection;
+
+/**
+ * Services designed to fetch collections of Cards
+ */
+@Service
+@Transactional
+public class CardSearch {
+    private final CardDAO cardDAO;
+
+    /**
+     * Uses constructor-based dependency injection.
+     * @param cardDAO The Card's DAO
+     */
+    @Autowired
+    public CardSearch(CardDAO cardDAO) {
+        this.cardDAO = cardDAO;
+    }
+    
+    /**
+	 * Get all Cards in DB
+	 * 
+	 * @return Collection of all cards
+	 */
+	public Collection<Card> getAll() {
+		return cardDAO.findAll();
+	}
+}
