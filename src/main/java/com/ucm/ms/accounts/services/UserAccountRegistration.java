c1fcaf784d7db84d90e4421eca5a760531f90fdd
Joshua Podhola <joshua.podhola@smoothstack.com>
2021-05-30 22:24:09 -0400

    Added logic to add UserAccountRegistration to database. Needs to be integration tested (hard to do when JWTs expire).

--- src/main/java/com/ucm/ms/accounts/services/UserAccountRegistration.java
+++ src/main/java/com/ucm/ms/accounts/services/UserAccountRegistration.java
@@ -4,11 +4,13 @@ import com.ucm.lib.dao.UserDAO;
 import com.ucm.lib.entities.User;
 import com.ucm.lib.services.JwtUtil;
 import com.ucm.ms.accounts.dao.AccountDAO;
+import com.ucm.ms.accounts.dao.UserAccountConfirmationDAO;
 import com.ucm.ms.accounts.dao.UserAccountDAO;
 import com.ucm.ms.accounts.dto.RegisterUserAccountDTO;
 import com.ucm.ms.accounts.dto.RegisterUserAccountRespDTO;
 import com.ucm.ms.accounts.entities.Account;
 import com.ucm.ms.accounts.entities.UserAccount;
+import com.ucm.ms.accounts.entities.UserAccountConfirmation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,9 @@ import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Random;
+import java.util.UUID;
 
 /**
  * Service for registration of user accounts.
@@ -29,13 +33,16 @@ public class UserAccountRegistration {
     private final UserDAO userDAO;
     private final AccountDAO accountDAO;
     private final JwtUtil jwtUtil;
+    private final UserAccountConfirmationDAO userAccountConfirmationDAO;
 
     @Autowired
-    public UserAccountRegistration(UserAccountDAO userAccountDAO, UserDAO userDAO, AccountDAO accountDAO, JwtUtil jwtUtil) {
+    public UserAccountRegistration(UserAccountDAO userAccountDAO, UserDAO userDAO, AccountDAO accountDAO,
+                                   JwtUtil jwtUtil, UserAccountConfirmationDAO userAccountConfirmationDAO) {
         this.userAccountDAO = userAccountDAO;
         this.userDAO = userDAO;
         this.accountDAO = accountDAO;
         this.jwtUtil = jwtUtil;
+        this.userAccountConfirmationDAO = userAccountConfirmationDAO;
     }
 
     public RegisterUserAccountRespDTO register(RegisterUserAccountDTO registerUserAccountDTO, String token) {
@@ -50,17 +57,35 @@ public class UserAccountRegistration {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No account with that ID.");
         }
 
-
         userAccount.setAccount(account);
         userAccount.setUser(user);
         userAccount.setBalance(new BigDecimal(0));
         userAccount.setAccountNumber(randomAccountNumber());
         userAccountDAO.save(userAccount);
+        UserAccountConfirmation userAccountConfirmation = generateConfirmation(userAccount);
+        userAccountConfirmationDAO.save(userAccountConfirmation);
         RegisterUserAccountRespDTO registerUserAccountRespDTO = new RegisterUserAccountRespDTO();
         registerUserAccountRespDTO.setAccountNumber(userAccount.getAccountNumber());
         registerUserAccountRespDTO.setAccountType(userAccount.getAccount().getType());
         registerUserAccountRespDTO.setAccountName(userAccount.getAccount().getName());
         return registerUserAccountRespDTO;
+    }
+
+    /**
+     * Generates a UserAccountConfirmation to the user. Does not save.
+     * @param userAccount The UserAccount to confirm.
+     * @return A UserAccountConfirmation.
+     */
+    protected UserAccountConfirmation generateConfirmation(UserAccount userAccount) {
+        UserAccountConfirmation userAccountConfirmation = new UserAccountConfirmation();
+        userAccountConfirmation.setUserAccount(userAccount);
+        userAccountConfirmation.setExpires(LocalDateTime.now().plusDays(1));
+        String s;
+        do {
+            s = UUID.randomUUID().toString();
+            userAccountConfirmation.setCode(s);
+        } while(userAccountConfirmationDAO.findFirstByCode(s) != null);
+        return userAccountConfirmation;
     }
 
     protected String randomAccountNumber() {
